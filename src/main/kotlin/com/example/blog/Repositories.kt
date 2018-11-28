d51bdb4550b064634e6f880717b32f020b5e2722
Yunseop Kim <undefined@mealant.com>
2018-11-28 19:03:59 +0900

    first commit

---
+++ src/main/kotlin/com/example/blog/Repositories.kt
@@ -0,0 +1,8 @@
+package com.example.blog
+import org.springframework.data.repository.CrudRepository
+
+interface ArticleRepository : CrudRepository<Article, Long> {
+	fun findAllByOrderByAddedAtDesc(): Iterable<Article>
+}
+
+interface UserRepository : CrudRepository<User, String>
